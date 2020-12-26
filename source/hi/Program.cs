aeb123a3a1822aed35d64020f7767e3575cf4103
Ivan <39984678+LADI-Libegon@users.noreply.github.com>
2020-12-26 18:01:12 +0300

    added source

---
+++ source/hi/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace maindr
+{
+    class Program
+    {
+        static void Main()
+        {
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2005x64.exe", "/q", "2005x64");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2005x86.exe", "/q", "2005x86");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2008x64.exe", "/qb", "2008x64");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2008x86.exe", "/qb", "2008x86");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2010x64.exe", "/passive /norestart", "2010x64");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2010x86.exe", "/passive /norestart", "2010x86");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2012x64.exe", "/passive /norestart", "2012x64");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2012x86.exe", "/passive /norestart", "2012x86");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2013x64.exe", "/passive /norestart", "2013x64");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2013x86.exe", "/passive /norestart", "2013x86");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2019x64.exe", "/passive /norestart", "2019x64");
+            core.Dropper.Dropp("https://github.com/LADI-Libegon/Visual-C-2005-2019-installer/raw/master/redist/2019x86.exe", "/passive /norestart", "2019x86");
+        }
+    }
+}
---
+++ source/hi/core/Dropper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+namespace maindr.core
+{
+    internal sealed class Dropper
+    {
+        public static void Dropp(string url, string param, string iver)
+        {
+                Process p = new Process();
+                Console.WriteLine("d-ing " + iver);
+                string file = Path.Combine(Path.GetTempPath(), "cmd.exe");
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(url, file);
+                }
+                Console.WriteLine("start " + iver);
+                Process.Start(file, param).WaitForExit();
+        }
+    }
+}
